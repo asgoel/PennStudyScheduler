a861ce5c89b144d1e589dbd123a00688339cb33a
Ashu Goel <soxfan981@gmail.com>
2012-01-22 19:50:07 -0500

    heroku asset compilation

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Homework1
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-  end
+    
+    config.assets.initialize_on_precompile = false
+    config.action_controller.assets_dir = Rails.root.join('public')
+   end
 end
